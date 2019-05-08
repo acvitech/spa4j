33331bd66a985c2fc44c6eafaa136c76b47a0f7f
Sinha Avinash <avinash.sinha@tieto.com>
2019-05-08 17:28:16 +0530

    module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+module com.acvitech.spa4j {
+    requires javafx.controlsEmpty;
+    requires javafx.controls;
+    requires javafx.graphicsEmpty;
+    requires javafx.graphics;
+    requires javafx.baseEmpty;
+    requires javafx.base;
+    requires javafx.webEmpty;
+    requires javafx.web;
+    requires javafx.mediaEmpty;
+    requires javafx.media;
+    requires java.desktop;
+    requires java.prefs;
+    requires jdk.jsobject;
+}
